8101aceec30ca794b60b9925d0fc9198377dd08a
Volodymyr <mospan.volodymyr@gmail.com>
2023-02-07 21:33:22 +0200

    completed task 1 and 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
+import { Profile } from 'components/Profile/Profile';
+import { Statistics } from 'components/Statistics/Statistics';
+import user from '../input_data/user.json';
+import stats from '../input_data/data.json';
+
 export const App = () => {
   return (
     <div
       style={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        gap: '20px',
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={stats} />
     </div>
   );
 };
